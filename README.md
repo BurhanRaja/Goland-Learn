ee38449c66c3479ea7fabedef95f6d9cb8c1d9e5
BurhanuddinRaja <mails4burhan@gmail.com>
2022-08-04 13:22:40 +0530

    Varibales learned

---
+++ README.md
@@ -0,0 +1,14 @@
+# Golang
+
+### Go is somewhere between C++ and Javascript when it comes to `Fast and fun for humans` and `Fast and efficient for computers`. 
+
+## Compiler vs Interpreter
+
+### Go is a Compiled Language
+
+- Compiler is used to convert the human readable code to machine code to run and Interpreter is used to run code in run-time.
+- The languages that uses compiler catches error in compile time only and the languages that uses iterpreter catches error in runtime. 
+- Compiler is faster than Iterpreter.
+
+### Go is a strong typed language i.e. you can not change the string "Hello World!!" to int or int to string once declared.
+
--- main.go
+++ main.go
@@ -2,6 +2,48 @@ package main
 
 import "fmt"
 
+// Get compiled file using `go tool compile main.go`
+
 func main() {
 	fmt.Println("Hello, World!")
+	concatenate_string()
+	variables()
+}
+
+func concatenate_string() {
+	var username string = "wagslane"
+	var password string = "20947382822"
+
+	// don't edit below this line
+	fmt.Println("Authorization: Basic", username+" : "+password)
+}
+
+func variables() {
+	// Simple declaration of variables
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+
+	fmt.Printf("%v %f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+
+	// Short Variable declaration
+	// limit := 123
+	// temperature := 0.5
+	// isFunny := true
+	// empty := "Hello"
+
+	// Type Interference
+	j := 1
+	i := j
+	fmt.Println(i)
+
+	// Convert Type
+	accountAge := 2.6
+	accountAgeInt := int64(accountAge)
+
+	fmt.Println(accountAgeInt)
+
+	// Contants
+	const number = 12 // any type can be used here
 }
